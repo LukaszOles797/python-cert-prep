1308ee34fc3ceac4196c55831f6c1013b59344ce
Łukasz Oleś <59345672+LukaszOles797@users.noreply.github.com>
2023-05-18 16:39:14 +0200

    Add display_digits function

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -4,10 +4,12 @@ segments = [["###", "#", "###", "###", "# #", "###", "###", "###", "###", "###"]
 ["# #", "#", "#  ", "  #", "  #", "  #", "# #", "  #", "# #", "  #"],
 ["###", "#","###", "###", "  #", "###", "###", "  #", "###", "###"]]
 
+def display_digits(number):
+    for i in range(5):
+        for ch in number:
+            print(segments[i][int(ch)], end=" ")
+        print()
 
-display_num = input("Please enter the number to display: ")
 
-for i in range(5):
-    for ch in display_num:
-        print(segments[i][int(ch)], end=" ")
-    print()
+number = input("Please enter the number to display: ")
+display_digits(number)
